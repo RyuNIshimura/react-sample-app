af80e33f7001eb350f441704e95c774c023b7641
RyuNIshimura <ryryrnsmr201@gmail.com>
2020-04-02 00:43:53 +0900

    add: Bootstrapの導入

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import Header from './Header';
 import Articles from './Articles';
+import Button from 'react-bootstrap/Button';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <Articles />
+    <Button variant="primary">Primary</Button>{' '}
   </React.StrictMode>,
   document.getElementById('root'),
 );
